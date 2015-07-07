848ade131d8de3259885513acac716fd4507e234
orxshi <orxan.shibli@gmail.com>
2015-07-07 16:41:18 -0600

    added oscAirfoil/UnifiedGrid

---
+++ cases/oscAirfoil/UnifiedGrid/macro.py
@@ -0,0 +1,44 @@
+from paraview.simple import *
+
+def createVTK (fileName, mainView):
+	vtk = LegacyVTKReader(FileNames=[fileName])
+	vtkDis = Show(vtk, mainView)
+	Hide(vtk, mainView)
+	vtkDis.SetRepresentationType('Surface With Edges')
+	return [vtk, vtkDis]
+
+def createThreshold (inpt, var, minVal, maxVal, mainView):
+	th = Threshold(Input=inpt)
+	th.Scalars = ['CELLS', var]
+	th.ThresholdRange = [minVal, maxVal]
+	thDis = Show(th, mainView)
+	thDis.SetRepresentationType('Surface With Edges')
+	return th
+
+paraview.simple._DisableFirstRenderCameraReset()
+
+mainView = GetActiveViewOrCreate('RenderView')
+mainView.InteractionMode = '2D'
+
+fileName1 = '/home/orhan/Documents/codeBaku/cases/oscAirfoil/UnifiedGrid/out/7_7_2015_16_6/Grid_0/allVTK_0.vtk' 
+fileName2 = '/home/orhan/Documents/codeBaku/cases/oscAirfoil/UnifiedGrid/out/7_7_2015_16_6/Grid_1/allVTK_0.vtk'
+fileName3 = '/home/orhan/Documents/codeBaku/cases/oscAirfoil/UnifiedGrid/out/tri.vtk'
+
+[vtk1,vtk1Dis] = createVTK(fileName1, mainView)
+[vtk2,vtk2Dis] = createVTK(fileName2, mainView)
+[vtk3,vtk3Dis] = createVTK(fileName3, mainView)
+
+Show(vtk3, mainView)
+vtk3Res = GetDisplayProperties(vtk3, mainView)
+ColorBy(vtk3Res, None)
+vtk3Res.DiffuseColor = [255, 0, 0]
+
+th1 = createThreshold (vtk1, 'iBlank', 4, 4, mainView)
+th2 = createThreshold (th1, 'trim', 0, 0, mainView)
+Hide(th1, mainView)
+
+th3 = createThreshold (vtk2, 'iBlank', 4, 4, mainView)
+th4 = createThreshold (th3, 'trim', 0, 0, mainView)
+Hide(th3, mainView)
+
+mainView.ResetCamera()
---
+++ cases/oscAirfoil/UnifiedGrid/src/main.cpp
@@ -0,0 +1,226 @@
+#include <petscsys.h>
+#include "../../../../src/Grid/Grid.h"
+#include "../../../../src/Output/Output.h"
+#include "../../../../src/Solid/Solid.h"
+#include "../../../../src/Coeffs/Coeffs.h"
+#include "../../../../src/Solver/Solver.h"
+#include "../../../../src/AFT/AFT.h"
+#include "Init/Init.h"
+#include "MovingGrid/OscAirfoil.h"
+#include "MovingGrid/StraightMovingAirfoil.h"
+#include "Output/Output.h"
+
+int main(int argc, char** argv)
+{
+    //PetscInitialize (&argc, &argv, NULL, NULL);
+    
+    Watch watchSteady;
+    Watch watchOscAirfoil;
+    
+    /*PetscMPIInt rank, n_procs;
+    MPI_Comm world = PETSC_COMM_WORLD;
+    MPI_Comm_rank (world, &rank);
+    MPI_Comm_size (world, &n_procs);*/
+    
+    string mainDir = createOutputDir();
+    
+    
+    
+    Grid bg (mainDir, 0);
+    bg.read_grid();
+    bg.set_grid();
+    
+    Grid ag (mainDir, 1);
+    ag.read_grid();
+    ag.set_grid();
+    
+    Grid finalGrid (mainDir, 3);
+    
+    OscInit oscInit;
+    oscInit.read();    
+    oscInit.init (ag);
+    oscInit.init (bg);
+    
+    vector<Grid> grs;    
+    grs.push_back(bg);
+    grs.push_back(ag);
+    
+    Solid solid0;
+    Solid solid1;
+    
+    solid0.phys = 2;
+    solid1.phys = 3;
+    
+    grs[0].setWallDistance(solid0);
+    grs[1].setWallDistance(solid1);
+        
+    for (int i=0; i<grs.size(); ++i)
+    {
+        cout << ">>> Building grid[" << i << "].cellADT" << endl;
+        
+        grs[i].cellADT.build (grs[i]);
+
+        cout << ">>> Built grid[" << i << "].cellADT" << endl;
+    }
+
+    cout << ">>> Identifying (non)active cells" << endl;
+
+    for (int i=0; i<grs.size(); ++i)
+    {
+        for (int j=0; j<grs.size(); ++j)
+        {
+            if (j != i)
+            {
+                grs[i].identifyIBlank (grs[j]);
+            }
+        }
+    }
+    
+    
+    //grs[0].outAllVTK(0);
+    //grs[1].outAllVTK(0);
+    
+    
+    
+    
+    AFT::aft (grs, finalGrid);
+    exit(-2);
+    
+    /*Grid gr (mainDir, 0);
+    OscInit oscInit;
+    
+    gr.read_grid();
+    gr.set_grid();
+
+    Solver solSteady (gr, "SOLVER-STEADY");
+    solSteady.read ("Solver/solSteady.dat");
+    
+    Solver solOscAirfoil (gr, "SOLVER-OSC-AIRFOIL");
+    solOscAirfoil.read ("Solver/solOscAirfoil.dat");
+
+    oscInit.read();
+    oscInit.init (gr);
+
+    // solve steady state
+    SMAirfoil sma (solSteady.dt);
+    OscAirfoil oa (solOscAirfoil.dt);
+    sma.read ("MovingGrid/smAirfoil.dat");
+    oa.read ("MovingGrid/oscAirfoil.dat");
+
+    Coeffs coeffs (gr, oscInit.rhoInf, oscInit.Mach, oa.MachAirfoil);
+    
+    sma.getAllFaceVelocities (gr);
+    watchSteady.start();
+    (solSteady.implicit) ? solSteady.impl(gr) : solSteady.expl(gr);
+    solSteady.petsc.finalize();
+    watchSteady.stop();    
+    
+    int countr = 0;
+    watchOscAirfoil.start();
+    
+    // solve osc airfoil
+    for (solOscAirfoil.time=0.; solOscAirfoil.time<solOscAirfoil.finalTime; solOscAirfoil.time+=solOscAirfoil.dt)
+    {
+        oa.setAngles (solOscAirfoil.time);
+        oa.getAllFaceVelocities (gr);
+        (solOscAirfoil.implicit) ? solOscAirfoil.impl(gr) : solOscAirfoil.expl(gr);
+        coeffs.getCoeffs (gr);
+        outLiftCoef (coeffs, oa.alpha, solOscAirfoil.time);
+        gr.outAllVTK (countr);
+        oa.moveGrid (gr);
+        
+        ++countr;
+    }
+    watchOscAirfoil.stop();    
+    
+    if (rank == MASTER_RANK)
+    {
+        gr.outAllTecplot();
+        gr.outAllVTK (0);
+        coeffs.out.close();
+        log (mainDir, watchSteady.elapsedTime, "elapsedTimeSteady", watchSteady.unit);
+        log (mainDir, watchOscAirfoil.elapsedTime, "elapsedTimeOscAirfoil", watchOscAirfoil.unit);
+        solSteady.log (gr.logDir);
+        solOscAirfoil.log (gr.logDir);
+        sma.log (gr.logDir);
+        oa.log (gr.logDir);
+    }
+    
+    PetscFinalize();*/
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include "Method/Method.h"
+
+void customUnsteady (Grid& gr)
+{
+	presetSingle();
+
+	gr.getMMVariables();
+    gr.getMovingFaceVelocity();
+    gr.apply_BCs();
+    
+    gr.alpha = 0.016;
+    gr.delAlpha = 0.;
+    
+    SSG (gr);
+
+    gr.steady = false;
+    gr.useCFL = false;
+    gr.kc = 0.0814;
+    gr.alphaMax = 2.51;
+    gr.alphaMean = 0.016;    
+    
+    double dt = gr.dt;
+    
+    for (double time=0.; time<52; time += dt)
+    {
+       	gr.time = time;        
+
+       	gr.getMMVariables();
+       	gr.getMovingFaceVelocity();
+       	gr.apply_BCs();        
+        
+		SSG (gr);        
+
+        gr.coeffs();
+        gr.rotateMesh();
+        
+        gr.outAllUnsteady();
+    }
+}
+
+int main(int argc, char** argv)
+{
+	Grid gr;
+
+    //cout << string(SPLITTER_LEN, '=') << endl;
+	
+	double startTime = getWallTime();
+    customUnsteady (gr);
+    double endTime = getWallTime();
+    getElapsedTime (startTime, endTime, gr.elapsedTime, gr.elapsedTimeUnit);
+    postset (gr);
+
+    return 0;
+}*/
