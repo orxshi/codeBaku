e8f060a3b53bf35823e12ed37d1e8185b9e82922
orxshi <orxan.shibli@gmail.com>
2015-09-05 04:00:22 -0600

    some additions

---
+++ src/AFT/Triangle.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   Triangle.h
+ * Author: orhan
+ *
+ * Created on July 18, 2015, 11:46 PM
+ */
+
+#ifndef AFT_TRIANGLE_H
+#define	AFT_TRIANGLE_H
+
+#define maxValArea 3.
+#define maxValAR 2.
+#define maxSkew 0.75
+#define cSkew 100./3.
+#define cAA (100./3./maxValArea)
+#define cAR (100./3./maxValAR)
+
+#endif	/* AFT_TRIANGLE_H */
+
---
+++ src/AFT/advanceFront.h
@@ -0,0 +1,44 @@
+/* 
+ * File:   advanceFront.h
+ * Author: orhan
+ *
+ * Created on July 18, 2015, 10:51 PM
+ */
+
+#ifndef ADVANCEFRONT_H
+#define	ADVANCEFRONT_H
+
+#define tolPreferExistingPoint 30.
+double scoreAB;
+double scoreNP1;
+double scoreNP2;
+double score;
+bool A_CPX_exists;
+bool B_CPX_exists;
+int iA_CPX;
+int iB_CPX;
+bool elAB;
+bool elNP1;
+bool elNP2;
+Point cnp1;
+Point cnp2;
+int iCnp1;
+int iCnp2;
+int iChosenPoint;
+bool isNewPoint;
+//bool A_CPX_exists_AB;
+//bool B_CPX_exists_AB;
+//int iA_CPX_AB;
+//int iB_CPX_AB;
+bool A_CPX_exists_NP1;
+bool B_CPX_exists_NP1;
+int iA_CPX_NP1;
+int iB_CPX_NP1;
+bool A_CPX_exists_NP2;
+bool B_CPX_exists_NP2;
+int iA_CPX_NP2;
+int iB_CPX_NP2;
+double pdis;
+
+#endif	/* ADVANCEFRONT_H */
+
